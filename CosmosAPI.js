777eea20e5a30711ef9ca48d862d325820ff2ea1
AshishSri2020 <57796818+AshishSri2020@users.noreply.github.com>
2019-11-16 15:33:00 -0600

    Update CosmosAPI.js

--- CosmosAPI.js
+++ CosmosAPI.js
@@ -1,59 +1,38 @@
-﻿//=====to connect azure database
+
+
+//===to connect azure database
+const { CosmosClient } = require("@azure/cosmos");
+const endpoint = "https://sampledb123.documents.azure.com:443/"; // Add your endpoint
+const key = "[FhHc2ssWgaENG4IJDGQKGmAG5RYHSy90Gj8U7kFvuP7M31f0WOGR4nEEQHkYBrTEEypzvhGmeQQuv6ujSeCBUw==]"; // Add the masterkey of the endpoint
+const client = new CosmosClient({ endpoint, key });
+
+var http = require('http')
+var port = process.env.PORT || 88;
+
+const databaseid = "sample database";
+const containerId = "sample collection";
 
 var express = require('express');
 var app = express();
 
 //=====end azure database========
 
-//app.get('/', function (req, res)
-//{
-
-//    //Promise.all([promise1, promise2, promise3]).then(function (values) {
-//    //    console.log(values);
-//    //});  
-//    //var str = FetchData();
-//    res.send(str.then(() => FetchData()));
-//   //res.send(str.all([promise1, promise2, promise3]).then(function (values) { console.log(values) }));
-//  });
-
-
 app.get('/', async (request, response) => {
     // awaiting Promises here
 
     // if you just await a single promise, you could simply return with it,
     // no need to await for it
- //   let result = await FetchData();
-//    let resultString = "";
-  //  let printresultString = [];
+    let result = await FetchData();
+    let resultString = "";
+    let printresultString = [];
       
- //   let i = 0;
+    let i = 0;
 
-    //for (var queryResult of result) {        
-      //  printresultString = result[i].content["Sheet1"];
-//        i = i + 1;
-  //  }
+   for (i = 0; i < result.length; i++) {
+        printresultString.push(result[i].content["Sheet1"]);
+    }
 
-    //for (i = 0; i < result.length; i++) {
-      //  printresultString.push(result[i].content["Sheet1"]);
-    //}
-
-    //for (i = 0; i <= printresultString.length; i++) {
-    //    resultString += printresultString[i].A + "<br/>";
-    //   //i = i + 1;
-   // }
-        //for (var queryResult of result) {
-          //    resultString = JSON.stringify('id:' + queryResult.id + ' , content: ' + queryResult.content);    
-            //  response.json(resultString);
-            // console.log(`\tQuery returned ${resultString}\n`);                          
-            //}       
-   
-    ////    //response.send(`\tQuery returned ${resultString}\n`);
-    //for (let i = 0; i < result.length; i++) {
-    //    printresultString.push(result[i].content["Sheet1"]);
-    //}
-
-    //response.json(printresultString);   
-    response.send("hi ashish");   
+    response.json(printresultString);   
     response.end();
 })
 
@@ -101,7 +80,3 @@ async function uploaddata(groupname, companyname, userid, fileName, content) {
 
 //FetchData() //.catch(handleError);;
 ////FetchData(); 
-
-
-
-
